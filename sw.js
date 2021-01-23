1a78bab9a5d5d1435a57c7b8019ceb974107e626
Subhankar Pal <subho.playboy10@gmail.com>
2021-01-24 03:23:19 +0530

    first commit

--- sw.js
+++ sw.js
@@ -22,7 +22,7 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => caches.match('index.html'));
+      return fetch(event.request).catch(() => caches.match('index.php'));
     })
   );
 });
