1ccbd8be9c0569e9a1eaf9f709a1100e78e4ee4a
Subhankar Pal <subho.playboy10@gmail.com>
2021-01-24 02:44:06 +0530

    sendgrid api shifted

--- rapi/ostatus.php
+++ rapi/ostatus.php
@@ -1,6 +1,6 @@
 <?php
 require 'db.php';
-require  dirname(dirname(__FILE__)) . '/api/lib/sendgrid_php/vendor/autoload.php';
+require dirname(dirname(__FILE__)) . '/app-assets/vendors/autoload.php';
 $getkey = $con->query("select * from setting")->fetch_assoc();
 define('ONE_KEY', $getkey['one_key']);
 define('ONE_HASH', $getkey['one_hash']);
